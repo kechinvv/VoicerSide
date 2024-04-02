e2e9c634b2f4d15045cd8551e4f761c39bbd5f47
kechinvv <valeraghst@gmail.com>
2024-04-02 14:47:50 +0300

    init start button

---
+++ src/main/kotlin/com/github/kechinvv/voicerside/VoicerIcons.kt
@@ -0,0 +1,8 @@
+package com.github.kechinvv.voicerside
+
+import com.intellij.openapi.util.IconLoader.getIcon
+import javax.swing.Icon
+
+object VoicerIcons {
+        val MIC: Icon = getIcon("/icons/mic.png", VoicerIcons::class.java)
+}
---
+++ src/main/kotlin/com/github/kechinvv/voicerside/actions/MicAction.kt
@@ -0,0 +1,16 @@
+package com.github.kechinvv.voicerside.actions
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+
+class MicAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+}
