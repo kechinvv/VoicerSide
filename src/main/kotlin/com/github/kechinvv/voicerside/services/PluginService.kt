cf98b6c6969c26489989360712979642dc7f6fb9
Evgeny Bukreev <ebukreev@ptsecurity.com>
2024-04-03 10:03:40 +0300

    text inserting improvements

--- src/main/kotlin/com/github/kechinvv/voicerside/services/PluginService.kt
+++ src/main/kotlin/com/github/kechinvv/voicerside/services/PluginService.kt
@@ -1,5 +1,6 @@
 package com.github.kechinvv.voicerside.services
 
+import ai.grazie.utils.capitalize
 import com.github.kechinvv.voicerside.recognition.ModelRunner
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.WriteAction
@@ -7,14 +8,15 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
 import java.util.concurrent.Future
+import kotlin.math.max
 
 
 @Service(Service.Level.PROJECT)
-class PluginService() {
+class PluginService {
 
-    // TODO: offset by cursor from editor
-    private var offset = 0
+    private val maxDocumentWidth = 120
 
     companion object {
         @JvmStatic
@@ -23,17 +25,44 @@ class PluginService() {
 
     fun editOpenedFile(editor: Editor, data: String) {
         val document = editor.document
-        val dataNl = data + "\n"
-        WriteCommandAction.runWriteCommandAction(editor.project) {
-            document.insertString(document.textLength, data)
+
+        var lastLine = max(0, document.lineCount - 1)
+        val startOffset = document.getLineStartOffset(lastLine)
+        val endOffset = document.getLineEndOffset(lastLine)
+        val lineContentLength = document.getText(TextRange(startOffset, endOffset)).length
+
+        val modifiedData = when {
+            lineContentLength >= maxDocumentWidth -> System.lineSeparator() + data
+
+            lineContentLength + data.length < maxDocumentWidth -> data
+
+            else -> {
+                val index = maxDocumentWidth - lineContentLength
+                val beforeIndex = data.substring(0, index)
+                val beforeWs = beforeIndex.substringBeforeLast(" ")
+
+                beforeWs + System.lineSeparator() +
+                        data.substring(index + 1 - (beforeIndex.length - beforeWs.length))
+            }
         }
-        offset += dataNl.length
+
+        WriteCommandAction.runWriteCommandAction(editor.project) {
+            document.insertString(endOffset, modifiedData)
+            lastLine = max(0, document.lineCount - 1)
+            editor.caretModel.moveToOffset(document.getLineEndOffset(lastLine))
+        }
     }
 
     fun runRecognition(editor: Editor) {
         ModelRunner.runRecognition {
             println(it)
-            editOpenedFile(editor, it)
+
+            if (it.startsWith("\"text\"")) {
+                val content = it.substringAfter("\"text\" : \"")
+                    .dropLast(1).capitalize() + ". "
+
+                editOpenedFile(editor, content)
+            }
         }
     }
 
