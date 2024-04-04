513809e5a786549a330221a783fe2e49a2fc1ba3
kechin.vv <kechinv1@rambler.ru>
2024-04-04 18:58:50 +0300

    clean up

--- src/main/kotlin/com/github/kechinvv/voicerside/perform/PerformerShutdown.kt
+++ src/main/kotlin/com/github/kechinvv/voicerside/perform/PerformerShutdown.kt
@@ -1,8 +1,6 @@
 package com.github.kechinvv.voicerside.perform
 
-import com.github.kechinvv.voicerside.getCurrentLine
 import com.github.kechinvv.voicerside.services.PluginService
-import com.github.kechinvv.voicerside.write
 import com.intellij.openapi.editor.Editor
 
 class PerformerShutdown : AbstractPerformer("shutdown", false) {
