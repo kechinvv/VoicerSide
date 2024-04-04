a508dfd85aff078dbf906d0709f846aec1ff5a43
kechin.vv <kechinv1@rambler.ru>
2024-04-04 14:51:15 +0300

    gradleIntelliJPlugin downgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,30 +4,25 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java") // Java support
     kotlin("jvm") version "1.9.23"
-    id("org.jetbrains.intellij.platform") version "2.0.0-SNAPSHOT"
+    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
 }
 
 group = properties("pluginGroup").get()
 version = properties("pluginVersion").get()
 
-// Configure project's dependencies
 repositories {
     mavenCentral()
-
-    intellijPlatform {
-        defaultRepositories()
-    }
 }
 
-// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
+intellij {
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
+    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+}
+
 dependencies {
-//    implementation(libs.annotations)
-    intellijPlatform {
-        intellijIdeaCommunity("2023.3")
-    }
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
-    implementation("net.java.dev.jna:jna:5.14.0")
-    implementation("com.alphacephei:vosk:0.3.45")
 }
 
 // Set the JVM language level used to build the project.
@@ -36,24 +31,6 @@ kotlin {
 }
 
 
-intellijPlatform {
-    buildSearchableOptions = true
-    instrumentCode = false
-    projectName = project.name
-
-    pluginConfiguration {
-        id = properties("pluginId").get()
-        name = properties("pluginName").get()
-        version = properties("pluginVersion").get()
-        description = ""
-        changeNotes = """""".trimIndent()
-
-        ideaVersion {
-            sinceBuild = "223"
-            untilBuild = "241.*"
-        }
-    }
-}
 
 tasks {
     wrapper {
